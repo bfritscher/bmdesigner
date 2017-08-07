a78e4e471f79244af6c66873ae5fba5650050e89
Boris Fritscher <boris@fritscher.ch>
2017-08-07 17:18:25 +0200

    raven-js

--- src/main.js
+++ src/main.js
@@ -3,6 +3,8 @@
 import Vue from 'vue';
 import Vuetify from 'vuetify';
 import VueTimeago from 'vue-timeago';
+import Raven from 'raven-js';
+import RavenVue from 'raven-js/plugins/vue';
 
 import App from './App';
 import router from './router';
@@ -17,6 +19,12 @@ Vue.use(VueTimeago, {
     'en-US': require('vue-timeago/locales/en-US.json'),
   },
 });
+
+Raven
+    .config('https://a3e9d60494a249d4bba6e6244380e411@sentry.j42.org/15')
+    .addPlugin(RavenVue, Vue)
+    .install();
+
 Vue.config.productionTip = false;
 
 /* eslint-disable no-new */
