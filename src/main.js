795aabea5a450f8037f9921e8acc7a133f640ca1
Boris Fritscher <boris@fritscher.ch>
2017-08-09 08:58:53 +0200

    raven filter dev

--- src/main.js
+++ src/main.js
@@ -21,9 +21,58 @@ Vue.use(VueTimeago, {
 });
 
 Raven
-    .config('https://a3e9d60494a249d4bba6e6244380e411@sentry.j42.org/15')
-    .addPlugin(RavenVue, Vue)
-    .install();
+  .config('https://a3e9d60494a249d4bba6e6244380e411@sentry.j42.org/15', {
+    environment: process.env.NODE_ENV,
+    release: 'TODO',
+    shouldSendCallback: (data) => {
+      if (process.env.NODE_ENV === 'development') {
+        console.debug(data);
+        return false;
+      }
+      return true;
+    },
+    ignoreErrors: [
+      // Random plugins/extensions
+      'top.GLOBALS',
+      // See: http://blog.errorception.com/2012/03/tale-of-unfindable-js-error. html
+      'originalCreateNotification',
+      'canvas.contentDocument',
+      'MyApp_RemoveAllHighlights',
+      'http://tt.epicplay.com',
+      'Can\'t find variable: ZiteReader',
+      'jigsaw is not defined',
+      'ComboSearch is not defined',
+      'http://loading.retry.widdit.com/',
+      'atomicFindClose',
+      // Facebook borked
+      'fb_xd_fragment',
+      // ISP "optimizing" proxy - `Cache-Control: no-transform` seems to
+      // reduce this. (thanks @acdha)
+      // See http://stackoverflow.com/questions/4113268
+      'bmi_SafeAddOnload',
+      'EBCallBackMessageReceived',
+      // See http://toolbar.conduit.com/Developer/HtmlAndGadget/Methods/JSInjection.aspx
+      'conduitPage',
+    ],
+    ignoreUrls: [
+      // Facebook flakiness
+      /graph\.facebook\.com/i,
+      // Facebook blocked
+      /connect\.facebook\.net\/en_US\/all\.js/i,
+      // Woopra flakiness
+      /eatdifferent\.com\.woopra-ns\.com/i,
+      /static\.woopra\.com\/js\/woopra\.js/i,
+      // Chrome extensions
+      /extensions\//i,
+      /^chrome:\/\//i,
+      // Other plugins
+      /127\.0\.0\.1:4001\/isrunning/i,  // Cacaoweb
+      /webappstoolbarba\.texthelp\.com\//i,
+      /metrics\.itunes\.apple\.com\.edgesuite\.net\//i,
+    ],
+  })
+  .addPlugin(RavenVue, Vue)
+  .install();
 
 Vue.config.productionTip = false;
 
