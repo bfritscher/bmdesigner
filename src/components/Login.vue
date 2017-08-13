fc4f24a80616cb4406058b2340ecb0ecc5e400ac
Boris Fritscher <boris@fritscher.ch>
2017-08-13 22:15:44 +0200

    nicer login

--- src/components/Login.vue
+++ src/components/Login.vue
@@ -1,6 +1,15 @@
 <template>
   <div>
-    <div id="firebaseui-auth-container" v-show="!$store.state.currentUser"></div>
+    <v-layout row>
+      <v-flex md6 offset-md3>
+        <v-card>
+          <v-alert info :value="hasInviteToken">
+            Login or create an account to which you want to add the shared Business Model Canvas.
+          </v-alert>
+          <div id="firebaseui-auth-container" v-show="!$store.state.currentUser"></div>
+        </v-card>
+      </v-flex>
+    </v-layout>
   </div>
 </template>
 
@@ -9,12 +18,20 @@ import { startLoginUI } from '@/utils/firebase';
 
 export default {
   name: 'login',
+  data() {
+    return {
+      hasInviteToken: false,
+    };
+  },
   mounted() {
+    this.hasInviteToken = localStorage.getItem('inviteToken');
     startLoginUI();
   },
 };
 </script>
 
 <style>
-
+.mdl-card.mdl-shadow--2dp.firebaseui-container.firebaseui-id-page-sign-in {
+  box-shadow: none;
+}
 </style>
