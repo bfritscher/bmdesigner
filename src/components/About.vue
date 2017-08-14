e8b64925219fabbce9c629007da8c18b00aa10a3
Boris Fritscher <boris@fritscher.ch>
2017-08-14 08:11:45 +0200

    enable raven fix ssl

--- src/components/About.vue
+++ src/components/About.vue
@@ -1,6 +1,6 @@
 <template>
   <div style="position: absolute; top:0; left:0; right:0; bottom:0;">
-    <iframe frameborder="0" scrolling="no" id="userreport-forum" src="http://feedback.userreport.com/1ee24ff8-1aef-4308-bd32-dee0f3da0f96/"></iframe>
+    <iframe frameborder="0" scrolling="no" id="userreport-forum" src="//feedback.userreport.com/1ee24ff8-1aef-4308-bd32-dee0f3da0f96/"></iframe>
   </div>
 </template>
 
--- src/main.js
+++ src/main.js
@@ -5,10 +5,8 @@ import Vuetify from 'vuetify';
 import VueTimeago from 'vue-timeago';
 import humanFormat from 'human-format';
 import { auth, db } from '@/utils/firebase';
-/*
 import Raven from 'raven-js';
 import RavenVue from 'raven-js/plugins/vue';
-*/
 
 import App from './App';
 import router from './router';
@@ -24,22 +22,21 @@ Vue.use(VueTimeago, {
 });
 
 Vue.filter('humanformat', input => (isNaN(input) ? input : humanFormat(input)));
-/*
 Raven
   .config('https://a3e9d60494a249d4bba6e6244380e411@sentry.j42.org/15', {
     environment: process.env.NODE_ENV,
+    /*
     release: 'TODO',
     shouldSendCallback: (data) => {
       if (process.env.NODE_ENV === 'development') {
-        console.debug(data);
         return false;
       }
       return true;
     },
+    */
   })
   .addPlugin(RavenVue, Vue)
   .install();
-  */
 
 Vue.config.productionTip = false;
 
