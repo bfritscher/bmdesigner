3b04eece0c805da02bb309887b17b3aaf3bdec46
Boris Fritscher <boris@fritscher.ch>
2017-08-21 14:28:31 +0200

    sort home screen by recent

--- src/components/Home.vue
+++ src/components/Home.vue
@@ -4,7 +4,7 @@
       <v-btn v-if="$store.state.currentUser" fab primary dark class="floating-action" @click.native="createNewCanvas">
         <v-icon>add</v-icon>
       </v-btn>
-      <v-flex xs12 sm6 md4 xl3 v-for="({info, settings={}}, key) in user.projects" :key="key" v-if="isShown(info, settings)">
+      <v-flex xs12 sm6 md4 xl3 v-for="({info, settings={}, key}) in projects" :key="key">
         <v-card class="model">
           <v-card-media @click.native="$router.push({name:'bmc', params: {id: key}})" :contain="!!info.logoImage" :class="{'default-background': !info.logoImage}" :style="{'background-color': info.logoColor ? info.logoColor : colorHash(info.name)}" :src="info.logoImage ? info.logoImage : require('@/assets/default_bmc_logo_background.jpg')">
             <div class="left-icons" v-if="info.updatedAt">
@@ -60,17 +60,25 @@ export default {
     ...mapState({
       user: 'user',
     }),
+    projects() {
+      const projects = Object.keys(this.user.projects || {}).map((key) => {
+        const p = this.user.projects[key];
+        p.key = key;
+        return p;
+      }).filter((p) => {
+        if (this.$route.name === 'favorites') {
+          return p.settings.fav;
+        }
+        return true;
+      });
+      projects.sort((a, b) => new Date(b.info.updatedAt) - new Date(a.info.updatedAt));
+      return projects;
+    },
   },
   methods: {
     ...mapActions(['createNewCanvas', 'canvasInfoUpdate', 'canvasUserSettingsUpdate']),
     colorHash(input) {
       return colorHash.hex(input);
-    },
-    isShown(info, settings) {
-      if (this.$route.name === 'favorites') {
-        return settings.fav;
-      }
-      return true;
     },
     toggleFav(canvasKey, fav) {
       this.canvasUserSettingsUpdate({
