39c7983c9c4e02dfe8c74977e5b929fd54ab22d2
Boris Fritscher <boris@fritscher.ch>
2018-04-26 10:07:42 +0200

    add new uber and airbnb game

--- src/components/Play.vue
+++ src/components/Play.vue
@@ -1,7 +1,9 @@
 <template>
     <div>
         <div>
-            <v-btn @click.native="playGame">Nespresso</v-btn>
+            <v-btn @click.native="playGame('nespresso')">Nespresso</v-btn>
+            <v-btn @click.native="playGame('uber')">Uber</v-btn>
+            <v-btn @click.native="playGame('airbnb')">Airbnb</v-btn>
         </div>
         <iframe src="https://docs.google.com/forms/d/e/1FAIpQLSfoS2yHOUHkY3J4MwaLWRRwYy2HZOWXlhIHPtpkEVvXy2W2Fw/viewform?embedded=true" style="position: absolute; top:120px; left:0; right:0; bottom:0;width:100%;height:100%;" frameborder="0" marginheight="0" marginwidth="0">Loading...</iframe>
     </div>
@@ -10,10 +12,17 @@
 <script>
 import * as types from '@/store/mutation-types';
 import nespresso from '@/assets/nespresso.json';
+import uber from '@/assets/uber.json';
+import airbnb from '@/assets/airbnb.json';
 
 export default {
   methods: {
-    playGame() {
+    playGame(gameName) {
+      const games = {
+        nespresso,
+        uber,
+        airbnb,
+      };
       this.$store.commit(types.LAYOUT_UPDATE, { showLoading: 'Preparing game...' });
       fetch(
         'https://us-central1-bmdesigner-50d6c.cloudfunctions.net/importJSONProject',
@@ -23,7 +32,7 @@ export default {
             'Content-Type': 'application/json',
           },
           method: 'POST',
-          body: JSON.stringify(nespresso),
+          body: JSON.stringify(games[gameName]),
         },
       )
         .then(res => res.text())
