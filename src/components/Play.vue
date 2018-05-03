419902323257b88d11b2196fb61674402d34e06b
Boris Fritscher <boris@fritscher.ch>
2018-05-03 09:16:22 +0200

    add apple game

--- src/components/Play.vue
+++ src/components/Play.vue
@@ -4,6 +4,7 @@
             <v-btn @click.native="playGame('nespresso')">Nespresso</v-btn>
             <v-btn @click.native="playGame('uber')">Uber</v-btn>
             <v-btn @click.native="playGame('airbnb')">Airbnb</v-btn>
+            <v-btn @click.native="playGame('apple')">Apple</v-btn>
         </div>
         <iframe src="https://docs.google.com/forms/d/e/1FAIpQLSfoS2yHOUHkY3J4MwaLWRRwYy2HZOWXlhIHPtpkEVvXy2W2Fw/viewform?embedded=true" style="position: absolute; top:120px; left:0; right:0; bottom:0;width:100%;height:100%;" frameborder="0" marginheight="0" marginwidth="0">Loading...</iframe>
     </div>
@@ -14,6 +15,7 @@ import * as types from '@/store/mutation-types';
 import nespresso from '@/assets/nespresso.json';
 import uber from '@/assets/uber.json';
 import airbnb from '@/assets/airbnb.json';
+import apple from '@/assets/apple.json';
 
 export default {
   methods: {
@@ -22,6 +24,7 @@ export default {
         nespresso,
         uber,
         airbnb,
+        apple,
       };
       this.$store.commit(types.LAYOUT_UPDATE, { showLoading: 'Preparing game...' });
       fetch(
